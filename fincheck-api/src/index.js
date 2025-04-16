fe6c6a67a1abac9c7cc9bc38f893404db2173abe
Eric Martins <ericmartis123@gmail.com>
2025-04-16 15:31:30 -0300

    Checkpoint: rota protegida, dashboard funcional, tailwind ok

--- fincheck-api/src/index.js
+++ fincheck-api/src/index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
 import authRoutes from "./routes/authRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
 
 dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
--- fincheck-api/src/routes/transactionRoutes.js
+++ fincheck-api/src/routes/transactionRoutes.js
@@ -1,22 +1,21 @@
-import express from 'express'
+import express from "express";
 import {
   createTransaction,
   getTransactions,
   updateTransaction,
-  deleteTransaction
-} from '../controllers/transactionController.js'
-import { authMiddleware } from '../middlewares/authMiddleware.js'
-import { getSummary } from '../controllers/transactionController.js'
+  deleteTransaction,
+} from "../controllers/transactionController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { getSummary } from "../controllers/transactionController.js";
 
-
-const router = express.Router()
+const router = express.Router();
 
 // Todas as rotas abaixo exigem token (usuário logado)
-router.use(authMiddleware)
-router.get('/summary', getSummary)
-router.post('/', createTransaction)
-router.get('/', getTransactions)
-router.put('/:id', updateTransaction)
-router.delete('/:id', deleteTransaction)
+router.use(authMiddleware);
+router.get("/summary", getSummary);
+router.post("/", createTransaction);
+router.get("/", getTransactions);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
 
-export default router
+export default router;
